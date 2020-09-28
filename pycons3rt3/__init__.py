6e209b7cac7c1c84cf8c210d0c151baa25594c65
Joe Yennaco <joe@joeyennaco.com>
2020-09-28 18:13:31 -0400

    Toned down the __init__.py file to not auto import too much

--- pycons3rt3/__init__.py
+++ pycons3rt3/__init__.py
@@ -18,53 +18,14 @@ pycons3rt3
 :license: ISC, see LICENSE for more details.
 
 """
-from . import osutil
-from . import aliasip
-from . import aws_metadata
-from . import bash
-from . import nexus
-from . import openssl
-from . import slack
-from . import ssh
-from . import pygit
-from . import pyjavakeys
-from . import windows
 
-
-from .cons3rtapi import Cons3rtApi
-from .bash import run_command, run_remote_command
-from .deployment import Deployment
-from .ec2util import EC2Util
-from .logify import Logify
-from .s3util import S3Util
-
-
-from .exceptions import \
-    DeploymentError, \
-    Cons3rtApiError, \
-    CommandError, \
-    S3UtilError
 
 __title__ = 'pycons3rt3'
 __name__ = 'pycons3rt3'
 __all__ = [
-    'aliasip',
-    'bash',
-    'aliasip',
     'cons3rtapi',
     'deployment',
-    'ec2util',
     'exceptions',
-    'images',
     'logify',
-    'nexus',
-    'openssl',
-    'osutil',
-    'pygit',
-    'pyjavakeys',
-    'slack',
-    'ssh',
-    's3util',
-    'slack',
-    'windows'
+    'osutil'
 ]
