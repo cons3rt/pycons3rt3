c05088d56e7bc8fdfb46d5a60b523d0ebd48ea7b
Joe Yennaco <joe@joeyennaco.com>
2021-07-21 22:11:36 -0400

    Fixed ansible-vault encryption/decryption, and added a vault-id option

--- pycons3rt3/ansiblevault.py
+++ pycons3rt3/ansiblevault.py
@@ -21,12 +21,12 @@ __author__ = 'Joe Yennaco'
 mod_logger = Logify.get_name() + '.ansiblevault'
 
 
-def ansible_vault_decrypt_file(encrypted_file, decrypted_file, password_file):
-    """Decrypts a file using ansible-vault and a password file
+def ansible_vault_decrypt_file(encrypted_file, password_file, vault_id=None):
+    """Decrypts a file using ansible-vault and a password file, optionally a Vault ID
 
     :param encrypted_file: (str) path to encrypted file
-    :param decrypted_file: (str) path to decrypted file
-    :param password_file: (str) path to the password file for decryption
+    :param password_file: (str) path to the Ansible Vault password file
+    :param vault_id: (str) Ansible Vault ID to provide along with the password file
     :return: True (if successful)
     :raises: CommandError
     """
@@ -36,32 +36,33 @@ def ansible_vault_decrypt_file(encrypted_file, decrypted_file, password_file):
     if not os.path.isfile(encrypted_file):
         raise CommandError('Encrypted file not found: {f}'.format(f=encrypted_file))
 
-    # Remove decrypted file if it exists
-    if os.path.isfile(decrypted_file):
-        os.remove(decrypted_file)
-
-    # First, copy the encrypted file into the desired output place
-    shutil.copy2(encrypted_file, decrypted_file)
+    # Determine the command based on Vault ID provided or not
+    if vault_id:
+        log.info('Decrypting file [{f}] with vault ID and password file [{v}@{p}]'.format(
+            f=encrypted_file, v=vault_id, p=password_file))
+        command = ['ansible-vault', 'decrypt', '--vault-id', '{v}@{p}'.format(v=vault_id, p=password_file)]
+    else:
+        log.info('Decrypting file [{f}] with password file [{p}]'.format(f=encrypted_file, p=password_file))
+        command = ['ansible-vault', 'decrypt', encrypted_file, '--vault-password-file', password_file]
 
     # Decrypt the file in-place
-    command = ['ansible-vault', 'decrypt', decrypted_file, '--vault-password-file', password_file]
     try:
         result = run_command(command, timeout_sec=10.0)
     except CommandError as exc:
         raise CommandError('Problem decrypting file {f} with password file: {p}'.format(
-            f=decrypted_file, p=password_file)) from exc
+            f=encrypted_file, p=password_file)) from exc
     if result['code'] != 0:
         raise CommandError('ansible-vault exited with code: {c}'.format(c=str(result['code'])))
-    log.info('Created decrypted file: {d}'.format(d=decrypted_file))
+    log.info('Created decrypted file: {d}'.format(d=encrypted_file))
     return True
 
 
-def ansible_vault_encrypt_file(decrypted_file, password_file, encrypted_file=None):
-    """Encrypts a file using ansible-vault and a password file
+def ansible_vault_encrypt_file(decrypted_file, password_file, vault_id=None):
+    """Encrypts a file using ansible-vault and a password file, optionally a vault ID
 
     :param decrypted_file: (str) path to decrypted file
     :param password_file: (str) path to the password file for decryption
-    :param encrypted_file: (str) path to encrypted file, will append .enc if not provided
+    :param vault_id: (str) Ansible Vault ID to provide along with the password file
     :return: True (if successful)
     :raises: CommandError
     """
@@ -70,17 +71,17 @@ def ansible_vault_encrypt_file(decrypted_file, password_file, encrypted_file=Non
         raise CommandError('Password file not found: {f}'.format(f=password_file))
     if not os.path.isfile(decrypted_file):
         raise CommandError('Decrypted file not found: {f}'.format(f=decrypted_file))
-    if not encrypted_file:
-        encrypted_file = decrypted_file + '.enc'
 
-    # Remove encrypted file if it exists
-    if os.path.isfile(encrypted_file):
-        os.remove(encrypted_file)
+    # Determine the command based on Vault ID provided or not
+    if vault_id:
+        log.info('Encrypting file [{f}] with vault ID and password file [{v}@{p}]'.format(
+            f=decrypted_file, v=vault_id, p=password_file))
+        command = ['ansible-vault', 'encrypt', '--vault-id', '{v}@{p}'.format(v=vault_id, p=password_file)]
+    else:
+        log.info('Encrypting file [{f}] with password file [{p}]'.format(f=decrypted_file, p=password_file))
+        command = ['ansible-vault', 'encrypt', decrypted_file, '--vault-password-file', password_file]
 
-    # First, copy the decrypted file into the desired output place
-    shutil.copy2(decrypted_file, encrypted_file)
-
-    command = ['ansible-vault', 'encrypt', encrypted_file, '--vault-password-file', password_file]
+    # Encrypt the file in-place
     try:
         result = run_command(command, timeout_sec=10.0)
     except CommandError as exc:
@@ -88,5 +89,5 @@ def ansible_vault_encrypt_file(decrypted_file, password_file, encrypted_file=Non
             f=decrypted_file, p=password_file)) from exc
     if result['code'] != 0:
         raise CommandError('ansible-vault exited with code: {c}'.format(c=str(result['code'])))
-    log.info('Created encrypted file: {d}'.format(d=encrypted_file))
+    log.info('Created encrypted file: {d}'.format(d=decrypted_file))
     return True
